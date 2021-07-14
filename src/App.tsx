b2414a8c7faba09b44e95e65cdf4d986c102cf43
Tijani Fulani <Fulani07@gmail.com>
2021-07-14 14:37:49 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import Header from "./components/Header";
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Header />
+      <main>
+        {/* Ṛest of components will go here
+         */}
+      </main>
+    </>
+  );
+}
+
+export default App
